2601263726a7aece79c60f4fcb61e0f9d8a08d88
agarzonp <agarzonp@gmail.com>
2016-10-15 11:20:07 +0200

    Fix spelling error

--- octet/src/examples/invaderers/agarzonp/Gameplay/GameStatesDefs.h
+++ octet/src/examples/invaderers/agarzonp/Gameplay/GameStatesDefs.h
@@ -21,7 +21,7 @@ namespace agarzonp
 // All the specific states
 //#include "States/InitState.h"
 //#include "States/MenuState.h"
-#include "States/BatlleState.h"
+#include "States/BattleState.h"
 #include "States/PauseState.h"	
 //#include "States/RankingState.h"
 //#include "States/ExitState.h"
